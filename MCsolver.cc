5697f65e5f15c2fe77d163f5ebd1415a19a161dd
wta2001 <57104055+wta2001@users.noreply.github.com>
2023-08-06 06:05:50 +0800

    Add files via upload

--- MCsolver.cc
+++ MCsolver.cc
@@ -1,30 +1,30 @@
-#include "MCsolver.hh"
+#include "include/MCsolver.hh"
 
 namespace solver {
 MCsolver::MCsolver(Oper& H_in, Ket& psi0_in, arma::vec& tlist_in, arma::cx_cube& c_ops_in, arma::cx_cube& e_ops_in,
- int& ntraj, int& num_threads) :I(0,1), gen(rd()), Heff(H_in), tlist(tlist_in), c_ops(c_ops_in), e_ops(e_ops_in) {
+ int& ntraj, int& num_threads) :I(0,1), Heff(H_in), psi0(psi0_in), tlist(tlist_in), c_ops(c_ops_in), e_ops(e_ops_in), ntraj(ntraj),
+ ensemble(psi0_in.n_elem, ntraj), expect(tlist_in.n_elem, e_ops_in.n_slices, arma::fill::zeros) {
     for(arma::uword i = 0; i < c_ops.n_slices; ++i)
         Heff -= I/2.0*c_ops.slice(i).t()*c_ops.slice(i);
-    ensemble = arma::repmat(psi0_in, 1, ntraj);
-    expect = arma::zeros<arma::cx_mat>(tlist.n_elem, e_ops.n_slices);
     omp_set_num_threads(num_threads);
 }
 
 MCsolver::MCsolver(complex* H_data, complex* psi0_data, double* tlist_data, complex* c_ops_data, complex* e_ops_data,
- int& q_dim, int& n_time, int& n_c_ops, int& n_e_ops, int& ntraj, int& num_threads) :I(0,1), gen(rd()),
+ int& q_dim, int& n_time, int& n_c_ops, int& n_e_ops, int& ntraj, int& num_threads) :I(0,1),
  Heff(H_data, q_dim, q_dim, false, true), 
+ psi0(psi0_data, q_dim, false, true),
  tlist(tlist_data, n_time, false, true),
  c_ops(c_ops_data, q_dim, q_dim, n_c_ops, false, true),
- e_ops(e_ops_data, q_dim, q_dim, n_e_ops, false, true) {
+ e_ops(e_ops_data, q_dim, q_dim, n_e_ops, false, true),
+ ntraj(ntraj),
+ ensemble(q_dim, ntraj),
+ expect(n_time, n_e_ops, arma::fill::zeros) {
     for(arma::uword i = 0; i < c_ops.n_slices; ++i)
         Heff -= I/2.0*c_ops.slice(i).t()*c_ops.slice(i);
-    Ket psi0(psi0_data, q_dim, false, true);
-    ensemble = arma::repmat(psi0, 1, ntraj);
-    expect = arma::zeros<arma::cx_mat>(tlist.n_elem, e_ops.n_slices);
     omp_set_num_threads(num_threads);
 }
 
-Ket MCsolver::Propagate(Ket psi, double dt) {
+Ket MCsolver::Propagate(Ket psi, double dt, pcg32& rng) {
     arma::uword n = c_ops.n_slices;
     arma::vec weights(n+1);
     Ket product = psi - I*dt*Heff*psi;
@@ -37,7 +37,7 @@ Ket MCsolver::Propagate(Ket psi, double dt) {
         weights.at(i) = result;
     }
     std::discrete_distribution<> dist(weights.begin(), weights.end());
-    arma::uword idx = dist(gen);
+    arma::uword idx = dist(rng);
     if(idx == n) {
         return (psi - I*dt*Heff*psi)/std::sqrt(result_n);
     } else {
@@ -46,24 +46,26 @@ Ket MCsolver::Propagate(Ket psi, double dt) {
 }
 
 void MCsolver::Solve() {
-    for(arma::uword k = 0; k < e_ops.n_slices; ++k) 
-        expect(0, k) += arma::cdot(ensemble.col(0), e_ops.slice(k)*ensemble.col(0));
-    expect *= ensemble.n_cols;
-    arma::vec dt = arma::diff(tlist);
-    for(arma::uword i = 1; i < tlist.n_elem; ++i) {
-        #pragma omp parallel for
-        for(arma::uword j = 0; j < ensemble.n_cols; ++j) {
-            ensemble.unsafe_col(j) = Propagate(ensemble.col(j), dt(i-1));
-            for(arma::uword k = 0; k < e_ops.n_slices; ++k) {
-                complex to_add = arma::cdot(ensemble.col(j), e_ops.slice(k)*ensemble.col(j));
-                #pragma omp critical
-                {
-                expect(i, k) += to_add;
-                }
-            }
+    #pragma omp parallel for
+    for(arma::uword j = 0; j < ntraj; ++j) {
+        Ket psi = psi0;
+        arma::vec dt = arma::diff(tlist);
+        arma::cx_mat traj_expect(tlist.n_elem, e_ops.n_slices);
+        pcg32 rng(j);
+        for(arma::uword i = 1; i < tlist.n_elem; ++i) {
+            psi = Propagate(psi, dt(i-1), rng);
+            for(arma::uword k = 0; k < e_ops.n_slices; ++k)
+                traj_expect(i, k) = arma::cdot(psi, e_ops.slice(k)*psi);
+        }
+        ensemble.col(j) = psi;
+        #pragma omp critical
+        {
+            expect += traj_expect;
         }
     }
-    expect /= ensemble.n_cols;
+    expect /= ntraj;
+    for(arma::uword k = 0; k < e_ops.n_slices; ++k)
+        expect(0, k) = arma::cdot(psi0, e_ops.slice(k)*psi0);
 }
 
 std::vector<complex> MCsolver::get_expect() {
